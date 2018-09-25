ae63a4bec3b0817d97aa47d1f53b348534697d69
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-25 15:07:15 +1000

    add menu

--- main.rb
+++ main.rb
@@ -1,14 +1,24 @@
+# gem install terminal-basic-menu
+require 'terminal-basic-menu'
+
+# Create Menu
+menu = Menu.new
+menu.header = { text: "Pokemon Text Adventure", color: :red }
+
 # Introduction
-puts "Welcome to our awesome pokemon text adventure."
-puts "Please enter your name."
+body_text = "Welcome to our awesome pokemon text adventure.\n"
+body_text += "Please enter your name below."
+menu.body = { text: body_text }
+menu.display_menu
 name = gets.chomp
 
 # Choosing Starter pokemon
-puts "You are at proffesor Oak's labratory."
-puts "There are 3 pokeballs in front of you."
+body_text = "You are at proffesor Oak's labratory."
+body_text += "There are 3 pokeballs in front of you."
+body_text += "Please choose which pokeball to take. (1-3)"
 choice = 0
 while choice < 1 || choice > 3
-  puts "Please choose which pokeball to take. (1-3)"
+  menu.display_menu
   choice = gets.chomp.to_i
 end
 pokemon_choices = ["Squirtle", "Bulbasaur", "Charmeleon"]
