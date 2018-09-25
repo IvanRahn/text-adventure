9e66c2d1372c0713836883e68e9a7e66eee8ffa5
Michael G. McCallum <mikemccallum@michaels-mbp.ait-sydney.net.au>
2018-09-25 15:00:05 +1000

    mountains intro

---
+++ mountains.rb
@@ -0,0 +1,7 @@
+
+puts "You have arrived at Mystical Mountains. You notice that there is something mystical in the air which makes you feel confident for your next battle. You see a path leading up the main mountain which is filled with danger, do you wish to proceed?"
+case    gets.chomp.downcase.to_s 
+when "yes" then puts" You decide to continue and start the dangerous climb. You struggle along the way taking a few falls and bruises but your determination to catch them all keeps you going. You arrive at the half way mark when you hear a disturbing noise above you. A WILD POKEMON JUMPED AT YOU...IT'S A MANKEY!!! 
+What do you wish to do? Battle or run?" 
+when "no" then puts "No, grow up and continue your journey!"
+end 
