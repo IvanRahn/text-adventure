d87d344df2416866e993ddcce32e1f67fa34b452
Adam Demirel <adxm@msn.com>
2018-09-25 15:25:55 +1000

    Add keyword end.

--- main.rb
+++ main.rb
@@ -32,6 +32,7 @@ when "west" then puts "Feeling the sun on your skin you realize that today is go
 end
 case gets.chomp.downcase.to_s
 when "east" puts "You have arrived at Mystical Mountains. You notice that there is something mystical in the air which makes you feel confident for your next battle. You see a path leading up the main mountain which is filled with danger, do you wish to proceed?"
+end
 case    gets.chomp.downcase.to_s 
 when "yes" then puts" You decide to continue and start the dangerous climb. You struggle along the way taking a few falls and bruises but your determination to catch them all keeps you going. You arrive at the half way mark when you hear a disturbing noise above you. A WILD POKEMON JUMPED AT YOU...IT'S A MANKEY!!! 
 What do you wish to do? Battle or run?" 
