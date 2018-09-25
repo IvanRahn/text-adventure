c8555b3393fb5af45202024a9a0c4dc5a751faeb
Adam Demirel <adxm@msn.com>
2018-09-25 14:59:10 +1000

    DRY code up

--- main.rb
+++ main.rb
@@ -6,17 +6,15 @@ name = gets.chomp
 puts "..."
 
 # Choosing Starter pokemon
-puts "Thanks for joining Pokeworld today #{name}. You have stumbled into the lab, where professor Oak has created four starter pokemon for you to choose from - Squirtle, Pikachu, Charmander, Bulbasaur."
+puts "Thanks for joining Pokeworld today #{name}. You have stumbled into the lab, where professor Oak has created four starter pokemon for you to choose from - 1) Squirtle, 2) Pikachu, 3) Charmander, 4) Bulbasaur."
 puts
 puts "Who do you select? Choose wisely - as your life lies in it's fate."
-mypokey = gets.chomp.downcase
-
-if mypokey == "squirtle" then
-    puts "You have selected Squirtle, nice choice!"
-elsif mypokey == "pikachu" then
-    puts "You have selected Pikachu, great choice!"
-elsif mypokey == "charmander" then
-    puts "You have chosen Charmander, wise choice!"
-elsif mypokey == "bulbasaur" then
-    puts "you have chosen Bulbasaur, good choice!"
+    
+choice = 0
+while choice < 1 || choice > 4
+  puts "Please choose which pokeball to take. (1-4)"
+  choice = gets.chomp.to_i
 end
+pokemon_choices = ["Squirtle", "Bulbasaur", "Charmeleon", "Pikachu"]
+mypokey = pokemon_choices[choice]
+puts "You have chosen #{mypokey}, wise choice!"
